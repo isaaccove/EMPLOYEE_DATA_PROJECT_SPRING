8b3c2f78a0621d27540ed9f670cb24625ed7e14a
usere <isaaccove@gmail.com>
2020-02-18 11:24:47 +0100

    Employee Spring MVC project

---
+++ employee_data/src/main/java/com/employee/entity/Employee.java
@@ -0,0 +1,97 @@
+package com.employee.entity;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="employee_id")
+	private int employeeId;
+	
+	@Column(name="firstname")
+	private String firstname;
+	
+	@Column(name="lastname")
+	private String lastname;
+	
+	@Column(name="dateOfBirth")
+	private Date dateOfBirth;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="phonenumber")
+	private String phoneNUmber;
+
+	
+	
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Employee(String firstname, String lastname, Date dateOfBirth, String email, String phoneNUmber) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.dateOfBirth = dateOfBirth;
+		this.email = email;
+		this.phoneNUmber = phoneNUmber;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNUmber() {
+		return phoneNUmber;
+	}
+
+	public void setPhoneNUmber(String phoneNUmber) {
+		this.phoneNUmber = phoneNUmber;
+	}
+
+	
+}
